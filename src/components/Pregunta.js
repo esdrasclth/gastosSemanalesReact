1f1dfebcc78e25c9ab42c9a864d8aa6cdde53f9c
Esdras Clother <esdras.clother@outlook.com>
2021-05-29 14:55:15 -0600

    Creando funcion para definir presupuesto

--- src/components/Pregunta.js
+++ src/components/Pregunta.js
@@ -1,15 +1,26 @@
-import React, {Fragment} from 'react'
+import React, {Fragment, useState} from 'react'
 
 const Pregunta = () => {
+    const [cantidad, guardarCantidad] = useState(0);
+
+    const definirPresupuesto = e => {
+        guardarCantidad(parseInt(e.target.value, 10))
+    }
+
+    const agregarPresupuesto = e => {
+
+    }
+
     return ( 
         <Fragment>
             <h2>Coloca tu presupuesto</h2>
 
-            <form action="">
+            <form onSubmit={agregarPresupuesto}>
                 <input 
                     type="number"
                     className="u-full-width"
                     placeholder="Coloca tu presupuesto"
+                    onChange={definirPresupuesto}
                 />
 
                 <button 
